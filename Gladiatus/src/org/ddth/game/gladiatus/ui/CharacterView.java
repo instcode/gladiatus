bb512bb1fdea25e73ce2bbb47c90c2a53c438dce
instcode <instcode@c8c45cf3-c553-0410-8a66-f143dacca4bd>
2008-08-06 11:05:15 +0000

    Fix spelling & clean up junk code :|

--- Gladiatus/src/org/ddth/game/gladiatus/ui/CharacterView.java
+++ Gladiatus/src/org/ddth/game/gladiatus/ui/CharacterView.java
@@ -4,14 +4,13 @@ import org.ddth.game.gladiatus.ui.widget.Meter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
 public class CharacterView extends Composite {
 
-	//private String[] names = { "Strength", "Skill", "Algrity", "Constitution", "Charisma", "Armour", "Damage" };
-	private String[] names = { "Name", "Label", "Name", "label", "Name", "Label", "Damage" };
+	private String[] names = { "Strength", "Skill", "Agility", "Constitution", "Charisma", "Armour", "Damage" };
+	
 	/**
 	 * Create the composite
 	 * @param parent
@@ -20,14 +19,14 @@ public class CharacterView extends Composite {
 	public CharacterView(Composite parent, int style) {
 		super(parent, style);
 		final GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 3;
+		gridLayout.numColumns = 4;
 		setLayout(gridLayout);
 		addRow("Level", "7");
 		for (int i = 0; i < 5; i++) {
 			addRow(names[i], (i + 1)*20, 100);
 		}
-		addRow("ABC", "34%");
-		addRow("DEF", "94%");
+		addRow("Chance to Hit", "34%");
+		addRow("Chance to Double Hit", "94%");
 		//
 	}
 
@@ -37,16 +36,15 @@ public class CharacterView extends Composite {
 	
 	protected void addRow(String name, String value) {
 		Label lblName = new Label(this, SWT.NONE);
+		lblName.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
 		lblName.setText(name);
-
-		Canvas canvas = new Canvas(this, SWT.NONE);
-		canvas.setLayoutData(new GridData(100, 20));
 
 		Label lblValue = new Label(this, SWT.NONE);
 		lblValue.setText(value);
 	}
 	
 	protected int addRow(String name, int value, int max) {
+		new Label(this, SWT.NONE);
 		Label lblName = new Label(this, SWT.NONE);
 		lblName.setText(name);
 
@@ -54,7 +52,7 @@ public class CharacterView extends Composite {
 		meter.setMeter(value * 100 / max);
 		meter.setLayoutData(new GridData(200, 20));
 
-		Label lblValue = new Label(this, SWT.NONE);
+		Label lblValue = new Label(this, SWT.CENTER);
 		lblValue.setText(value + "/" + max);
 		return 0;
 	}
